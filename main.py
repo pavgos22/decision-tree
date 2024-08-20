f26aed02989d2d31d7d2c6f944845c781874c0bf
pavgos22 <pawgosciak@gmail.com>
2024-08-20 23:58:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from decision_tree import DecisionTree
+from random_forest import RandomForest
+from load_data import generate_data, load_titanic
+
+
+def main():
+    np.random.seed(123)
+
+    train_data, test_data = load_titanic()
+
+    dt = DecisionTree({"depth": 14})
+    dt.train(*train_data)
+    print("DecisionTree performance:")
+    print("Training data:")
+    dt.evaluate(*train_data)
+    print("Test data:")
+    dt.evaluate(*test_data)
+
+    rf = RandomForest({"ntrees": 16, "feature_subset": 2, "depth": 14})
+    rf.train(*train_data)
+    print("RandomForest performance:")
+    print("Training data:")
+    rf.evaluate(*train_data)
+    print("Test data:")
+    rf.evaluate(*test_data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ node.py
@@ -0,0 +1,105 @@
+import copy
+import numpy as np
+
+
+class Node:
+    def __init__(self):
+        self.left_child = None
+        self.right_child = None
+        self.feature_idx = None
+        self.feature_value = None
+        self.node_prediction = None
+
+    def gini_best_score(self, y, possible_splits):
+        best_gain = -np.inf
+        best_idx = 0
+
+        for possible_split in possible_splits:
+            left_child = y[:possible_split]
+            right_child = y[possible_split:]
+
+            left_pos = (left_child == 1).sum()
+            left_neg = (left_child == 0).sum()
+            right_pos = (right_child == 1).sum()
+            right_neg = (right_child == 0).sum()
+            left = left_pos + left_neg
+            right = right_pos + right_neg
+
+            if left == 0 or right == 0:
+                continue
+
+            gini_left = 1 - (left_pos/left)**2 - (left_neg/left)**2
+            gini_right = 1 - (right_pos/right)**2 - (right_pos/right)**2
+            gini_gain = 1 - left/(left+right) * gini_left - right/(left+right) * gini_right
+
+            if gini_gain > best_gain:
+                best_gain = gini_gain
+                best_idx = possible_split
+
+        return best_idx, best_gain
+
+    def split_data(self, X, y, idx, val):
+        left_mask = X[:, idx] < val
+        return (X[left_mask], y[left_mask]), (X[~left_mask], y[~left_mask])
+
+    def find_possible_splits(self, data):
+        possible_split_points = []
+        for idx in range(data.shape[0] - 1):
+            if data[idx] != data[idx + 1]:
+                possible_split_points.append(idx)
+        return possible_split_points
+
+    def find_best_split(self, X, y, feature_subset):
+        best_gain = -np.inf
+        best_split = None
+
+        for d in range(X.shape[1]):
+            order = np.argsort(X[:, d])
+            y_sorted = y[order]
+            possible_splits = self.find_possible_splits(X[order, d])
+            idx, value = self.gini_best_score(y_sorted, possible_splits)
+            if value > best_gain:
+                best_gain = value
+                best_split = (d, [idx, idx + 1])
+
+        if best_split is None:
+            return None, None
+
+        best_value = np.mean(X[best_split[1], best_split[0]])
+
+        return best_split[0], best_value
+
+    def predict(self, x):
+        if self.feature_idx is None:
+            return self.node_prediction
+        if x[self.feature_idx] < self.feature_value:
+            return self.left_child.predict(x)
+        else:
+            return self.right_child.predict(x)
+
+    def train(self, X, y, params):
+
+        self.node_prediction = np.mean(y)
+        if X.shape[0] == 1 or self.node_prediction == 0 or self.node_prediction == 1:
+            return True
+
+        self.feature_idx, self.feature_value = self.find_best_split(X, y, params["feature_subset"])
+        if self.feature_idx is None:
+            return True
+
+        (X_left, y_left), (X_right, y_right) = self.split_data(X, y, self.feature_idx, self.feature_value)
+
+        if X_left.shape[0] == 0 or X_right.shape[0] == 0:
+            self.feature_idx = None
+            return True
+
+        # max tree depth
+        if params["depth"] is not None:
+            params["depth"] -= 1
+        if params["depth"] == 0:
+            self.feature_idx = None
+            return True
+
+        self.left_child, self.right_child = Node(), Node()
+        self.left_child.train(X_left, y_left, copy.deepcopy(params))
+        self.right_child.train(X_right, y_right, copy.deepcopy(params))
